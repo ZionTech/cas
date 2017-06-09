8b64a2f522ecd104935a77c7339739acb561ab88
venkatakarthikb <venkatakarthikb@wavity.com>
2017-06-09 09:36:58 +0530

    Revert "stop add pgt to ticket registry for proxy tickets"

--- cas-server-core/src/main/java/org/jasig/cas/CentralAuthenticationServiceImpl.java
+++ cas-server-core/src/main/java/org/jasig/cas/CentralAuthenticationServiceImpl.java
@@ -286,7 +286,7 @@ public final class CentralAuthenticationServiceImpl extends AbstractCentralAuthe
             throws AuthenticationException, AbstractTicketException {
 
         final ServiceTicket serviceTicket =  this.ticketRegistry.getTicket(serviceTicketId, ServiceTicket.class);
-        
+
         if (serviceTicket == null || serviceTicket.isExpired()) {
             logger.debug("ServiceTicket [{}] has expired or cannot be found in the ticket registry", serviceTicketId);
             throw new InvalidTicketException(serviceTicketId);
@@ -307,8 +307,7 @@ public final class CentralAuthenticationServiceImpl extends AbstractCentralAuthe
         final ProxyGrantingTicket proxyGrantingTicket = factory.create(serviceTicket, authentication);
 
         logger.debug("Generated proxy granting ticket [{}] based off of [{}]", proxyGrantingTicket, serviceTicketId);
-        if(serviceTicket.getGrantingTicket().isRoot())
-        	this.ticketRegistry.addTicket(proxyGrantingTicket);
+        this.ticketRegistry.addTicket(proxyGrantingTicket);
 
         doPublishEvent(new CasProxyGrantingTicketCreatedEvent(this, proxyGrantingTicket));
 
